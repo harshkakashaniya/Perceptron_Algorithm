9061643fad2b90fe4d149e3834a787e608d7d369
harsh.kakashaniya <harsh.kakashniya@gmail.com>
2019-02-18 20:05:28 -0500

    Dev:Integrate random missclassified point choosing method

--- PLA.py
+++ PLA.py
@@ -7,7 +7,8 @@ import pickle
 import matplotlib.pyplot as plt
 import random
 N=100
-
+## data generation
+#-------------------------------------------------------------------------------
 data1=np.column_stack((np.ones(N),np.ones(N),np.ones(N)))
 
 for i in range(0,len(data1)):
@@ -18,7 +19,8 @@ for i in range(0,len(data1)):
         data1[i,2]=1
     else:
         data1[i,2]=-1
-
+#-------------------------------------------------------------------------------
+#data plotting
 count=0
 break_point=1
 for i in range(0,len(data1)):
@@ -27,31 +29,33 @@ for i in range(0,len(data1)):
     else:
         plt.plot(data1[i,0],data1[i,1],'or')
 
-def safer(num):
-    if (num>0):
-        return 1
-    else:
-        return -1
-
 
 def misclassified(data,w):
-    happy=1
+    break_point=1
     def safe(num):
         if (num>0):
             return 1
         else:
             return -1
     X=np.column_stack((np.ones(len(data1),dtype=int),data1[:,0],data1[:,1]))
+    misclassify=[]
     for i in range(0,len(data1)):
         mul=w*X.transpose()
         sign=safe(float(mul[0,i]))
         if(sign!=data1[i,2]):
-            a=i
-            break
-        if(i==len(data1)-1 and sign==data1[len(data1)-1,2]):
-            happy = 0
-            a=-5
-    return a,happy
+            misclassify=np.append([misclassify],[i])
+            #print(misclassify,'In loop')
+    if(len(misclassify)==0):
+        break_point = 0
+        a=-5
+        length=0
+    if(break_point == 1):
+        point=random.randint(1,len(misclassify))
+        a=int(misclassify[point-1])
+        print(misclassify)
+        print(len(misclassify),'misclassify number')
+        length=len(misclassify)
+    return a,break_point,length
 
 X=np.column_stack((np.ones(len(data1),dtype=int),data1[:,0],data1[:,1]))
 
@@ -63,14 +67,9 @@ w=np.mat([0.5,0,0])
 while(break_point==1):
     count=count+1
     print(count)
-    koyal,break_point=misclassified(data1,w)
+    koyal,break_point,length=misclassified(data1,w)
     if(koyal!=-5):
-        for i in range(0,len(data1)):
-            mul=w*X.transpose()
-            sign=safer(float(mul[0,i]))
-            if(sign!=data1[i,2]):
-                mis_counter+=1
-            #print(mis_counter)
+        mis_counter=mis_counter+length
         w_new=np.mat([0,0,0])
         w_new= w + X[koyal,:]*data1[koyal,2]
         #print(w_new)
@@ -84,22 +83,19 @@ while(break_point==1):
         plt.ylabel('y')
         plt.axis([-1,1,-1,1])
         plt.title('Data set')
-        plt.pause(0.001)
+        plt.pause(0.01)
 
 mis_avg=mis_counter/(len(data1)*count)
 print('Probability is',mis_avg)
-plt.show()
+#plt.show()
 
-'''
-w_new=np.mat([-0.3,2.32,-1.84])
 m=float(-w_new[0,1]/w_new[0,2])
 c=float(-w_new[0,0]/w_new[0,2])
 x_line1=np.mat([[-1],[1]])
 y_line1=m*x_line1+c
-plt.plot(x_line1,y_line1,'-y')
+plt.plot(x_line1,y_line1,'-m')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.axis([-1,1,-1,1])
 plt.title('Data set')
 plt.show()
-'''
